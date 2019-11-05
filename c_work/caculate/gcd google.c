aadd8e529bf8776c816feffbc54e4c3254af3db3
lixingqi <1628513134@qq.com>
2019-11-05 21:15:05 +0800

    third commit

--- c_work/caculate/gcd google.c
+++ c_work/caculate/gcd google.c
@@ -10,6 +10,8 @@
         if(n1%i==0 && n2%i==0)
         gcd = i;
     }
-    printf("G.C.D of %d and %d is %d", n1, n2, gcd);
-        return 0;
+    printf("G.C.D of %d and %d is %d.\n", n1, n2, gcd);
+    system("pause");
+    return 0;
     }
+//? 程序需要严格按数学公式或是结果导向型？
